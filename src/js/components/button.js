c2742848947586f18b69275e9349086c43907cd7
Daniel Kiefer <danielfmpinho@gmail.com>
2024-07-12 18:20:31 -0300

    chore: Update button component and styles
    This commit updates the button component by changing its HTML structure and CSS classes. It also adds a new button style for encryption and decryption. Additionally, the main.js file is refactored to remove unused code.

--- src/js/components/button.js
+++ src/js/components/button.js
@@ -1,10 +1,19 @@
-// Função para inicializar o evento de clique do botão
-function initializeButton() {
-  const button = document.getElementById("myButton");
+export function initializeButton(type, button) {
+  if (button) {
+    button.classList.add(type);
 
-  button
-    ? button.addEventListener("click", () => {
-        console.log("Button clicked");
-      })
-    : console.error("Button not found");
+    if (type === "code-button") {
+      button.textContent = "Criptografar";
+      button.addEventListener("click", () => {
+        console.log("Button Code clicked");
+      });
+    } else if (type === "decode-button") {
+      button.textContent = "Descriptografar";
+      button.addEventListener("click", () => {
+        console.log("Button Decode clicked");
+      });
+    }
+  } else {
+    console.error("Button not found");
+  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,36 @@
+import { initializeButton } from "./components/button.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   // Função para carregar o componente HTML de forma assíncrona
-  function loadComponent(url, elementId) {
+  function loadComponent(url, elementId, callback, type, buttonId) {
     fetch(url)
       .then((response) => response.text())
       .then((data) => {
-        document.getElementById(elementId).innerHTML = data;
-        // Após carregar o componente, chamar a função para inicializar o botão
-        initializeButton();
+        const container = document.createElement("div");
+        container.innerHTML = data;
+        const button = container.querySelector("button");
+        button.id = buttonId; // Atribui o ID específico ao botão
+        document.getElementById(elementId).appendChild(container);
+        if (callback) callback(type, button);
       })
       .catch((error) => {
         console.error("Erro ao carregar o componente:", error);
       });
   }
 
-  // Carregar o componente de botão
-  loadComponent("src/components/button.html", "app");
+  // Carregar os componentes de botão
+  loadComponent(
+    "src/components/button.html",
+    "app",
+    initializeButton,
+    "code-button",
+    "code-button"
+  );
+  loadComponent(
+    "src/components/button.html",
+    "app",
+    initializeButton,
+    "decode-button",
+    "decode-button"
+  );
 });
-
-// Função para inicializar o evento de clique do botão
-function initializeButton() {
-    const button = document.getElementById("myButton");
-  
-    button
-      ? button.addEventListener("click", () => {
-          console.log("Button clicked");
-        })
-      : console.error("Button not found");
-  }
-  
