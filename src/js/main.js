f2cfd5f1a6eaa3d05c4df5fbb32c9335fff4cb0f
Daniel Kiefer <danielfmpinho@gmail.com>
2024-07-13 18:05:03 -0300

    feat: Add codeText function to encrypt input text

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,46 @@
 import { initializeButton } from "./components/button.js";
 import { codeText, decodeText } from "./components/code.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  window.codeText = codeText; // Expondo para uso em button.js
-  window.decodeText = decodeText; // Expondo para uso em button.js
+document.addEventListener("DOMContentLoaded", async () => {
+  window.codeText = codeText;
+  window.decodeText = decodeText;
 
-  // Função para carregar o componente HTML de forma assíncrona
-  function loadComponent(url, elementId, callback, type, buttonId) {
-    fetch(url)
-      .then((response) => response.text())
-      .then((data) => {
-        const container = document.createElement("div");
-        container.innerHTML = data;
-        const button = container.querySelector("button");
-        button.id = buttonId; // Atribui o ID específico ao botão
-        document.getElementById(elementId).appendChild(container);
-        if (callback) callback(type, button);
-      })
-      .catch((error) => {
-        console.error("Erro ao carregar o componente:", error);
-      });
+  async function loadComponent(url, elementId, callback, type, buttonId) {
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+
+      const container = document.createElement("div");
+      container.style.width = "100%";
+      container.innerHTML = data;
+      const button = container.querySelector("button");
+      button.id = buttonId;
+
+      document.getElementById(elementId).appendChild(container);
+      if (callback) callback(type, button);
+    } catch (error) {
+      console.error("Erro ao carregar o componente:", error);
+    }
   }
 
-  // Carregar os componentes de botão
-  loadComponent(
-    "src/components/button.html",
-    "app",
-    initializeButton,
-    "code-button",
-    "code-button"
-  );
-  loadComponent(
-    "src/components/button.html",
-    "app",
-    initializeButton,
-    "decode-button",
-    "decode-button"
-  );
+  const appElement = document.getElementById("app");
+
+  const loadPromises = [
+    loadComponent(
+      "src/components/button.html",
+      "app",
+      initializeButton,
+      "code-button",
+      "code-button"
+    ),
+    loadComponent(
+      "src/components/button.html",
+      "app",
+      initializeButton,
+      "decode-button",
+      "decode-button"
+    ),
+  ];
+
+  await Promise.all(loadPromises);
 });
